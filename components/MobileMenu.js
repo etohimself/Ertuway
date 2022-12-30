cd49e6fe1dc04205fe5cd7cd9c72baada303b7eb
Ertuğrul CÖRE <ertugrulcore@gmail.com>
2022-12-30 11:28:20 +0300

    Mobile Navbar Updated
    Added dynamic My Account button. Home and My Cart buttons redirect properly now.

--- components/MobileMenu.js
+++ components/MobileMenu.js
@@ -3,10 +3,19 @@ import HomeIcon from "../components/Icons/HomeIcon";
 import UserIcon from "../components/Icons/UserIcon";
 import CartIcon from "../components/Icons/CartIcon";
 import CategoriesIcon from "../components/Icons/CategoriesIcon";
+import { useRouter } from "next/router";
+import { useContext } from "react";
+import { AuthContext } from "../contexts/authContext";
 
 function MobileMenu(props) {
+  const router = useRouter();
+  const { authData } = useContext(AuthContext);
+
   return (
-    <div className={styles.MobileMenuContainer}>
+    <div
+      className={styles.MobileMenuContainer}
+      onClick={() => router.push("/")}
+    >
       <div className={styles.MobileMenuButton}>
         <HomeIcon className={styles.MobileMenuIcons} />
         <p>Home</p>
@@ -15,11 +24,17 @@ function MobileMenu(props) {
         <CategoriesIcon className={styles.MobileMenuIcons} />
         <p>Categories</p>
       </div>
-      <div className={styles.MobileMenuButton}>
+      <div
+        className={styles.MobileMenuButton}
+        onClick={() => authData <= 0 && router.push("/login")}
+      >
         <UserIcon className={styles.MobileMenuIcons} />
-        <p>My Account</p>
+        <p>{authData.username ? `My Account` : `Sign In`}</p>
       </div>
-      <div className={styles.MobileMenuButton}>
+      <div
+        className={styles.MobileMenuButton}
+        onClick={() => router.push("/cart")}
+      >
         <CartIcon className={styles.MobileMenuIcons} />
         <p>My Cart</p>
       </div>
